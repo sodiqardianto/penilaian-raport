4145f5bea98cb6a4e3f8813e6972153a1c04dfff
budimp <budimp41@gmail.com>
2022-12-10 17:29:55 +0700

    Update Murid Model
    Update Murid Model

--- app/Models/Murid.php
+++ app/Models/Murid.php
@@ -9,12 +9,32 @@ class Murid extends Model
 {
     use HasFactory;
 
-    protected  $table = 'murid';
-
+    // Menentukan field yang bisa diisi
     protected $fillable = [
         'namamurid',
         'notelp',
         'jeniskelamin',
     ];
+
+    // Menentukan nama table yang digunakan
+    protected $table = 'murid';
+
+    // Menentukan relasi one-to-many dengan model Absen
+    public function absen()
+    {
+        return $this->hasMany('App\Absen');
+    }
+
+    // Menentukan relasi one-to-many dengan model KelasMurid
+    public function kelasmurid()
+    {
+        return $this->hasMany('App\KelasMurid');
+    }
+
+    // Menentukan relasi one-to-many dengan model Raport
+    public function raport()
+    {
+        return $this->hasMany('App\Raport');
+    }
 
 }
